32b5fad87439adf1c1df3b4e94d5868971ee9613
Md Shayon <mdshayon0@gmail.com>
2019-10-14 22:10:10 +0600

    something went wrong with opts in index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
-
 // EXAMPLE CODE FROM https://discord.js.org/#/
 const Discord = require('discord.js');
+const parseArgs = require('minimist');
 const client = new Discord.Client();
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -10,11 +11,47 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     console.log(msg.content);
+
+    // EVERYTHING WILL BE SPLIT IN ARRAY
+    // https://www.youtube.com/watch?v=qXzJNEVoQiY&list=PLbyS_i8haRArHUCyChjNaMg1BC3MHdPp7
+    const msgarr = msg.content.split(' ');
+    console.log(msgarr);
+
+    // IT CONTAIN INSIDE A OBJECT containing array
+    let command = parseArgs(msgarr);
+    console.log(command);
+
+    let dltUscoreCommand = command._;
+    let opts = parseArgs(msgarr);
+    delete opts._
+    console.log("Delete underscore in the array  : "+ dltUscoreCommand);
+    console.log("Options  : " + opts);
     
+    
+    
+    
+    
+  /*
   if (msg.content === 'ping') {
     //   IT MEANS IF WE WRITE PING IN OUR SERVER MESSAGE THE BOT REPLIED PONG
     // AND  IF I WRITE ANY MESSAGE IT WILL LOG TO CONSOLE
     msg.reply('Pong!');
+  }*/
+
+  switch(dltUscoreCommand[0]){
+      case 'Hi':
+          let sayHi =()=>{
+              msg.reply('Hello!');
+          }
+        //   IT THERE IS A DELAY OPTION IT WILL WAIT FOR DELAY
+          if('delay' in opts){
+              setTimeout(sayHi, opts.delay);
+              console.log("delayed");
+              
+          }else{
+              sayHi()
+          }
+          break;
   }
 });
 
