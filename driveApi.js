84ab0d3d758c6d854a505fe8e2bf347789eb93a6
Md Shayon <mdshayon0@gmail.com>
2019-10-19 21:34:25 +0600

    successfully get the folder

--- driveApi.js
+++ driveApi.js
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=gGSJpp6_ax0&t=440s
 // ALL MODULES
 const {google } = require('googleapis');
 // https://github.com/googleapis/google-api-nodejs-client#media-uploads
@@ -32,3 +33,25 @@ jwToken.authorize((authErr) =>{
 
 // MAKE REQUEST TO GOOGLE DRIVE WEB SERVICE
 // https://developers.google.com/drive/api/v3/reference/files/list?apix=true
+// list file in speciifcs folder
+var folder = "1HMoM6ynAWRrFJuQyX2QeCoSXOnSs4ZPQ";
+drive.files.list({
+
+  auth: jwToken,
+  pageSize: 10,
+  q: "'" + folder + "' in parents and trashed=false",
+  fields: 'files(id, name)',
+}, (err, {
+  data
+}) => {
+  if (err) return console.log('The API returned an error: ' + err);
+  const files = data.files;
+  if (files.length) {
+    console.log('Files:');
+    files.map((file) => {
+      console.log(`${file.name} (${file.id})`);
+    });
+  } else {
+    console.log('No files found.');
+  }
+});
