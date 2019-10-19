e1df18efa1e589868fc4d54cadc83c894fab5885
Md Shayon <mdshayon0@gmail.com>
2019-10-19 17:24:55 +0600

    remove client id

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 
 const {google} = require('googleapis');
 const keys = require('./config/keys.json');
+const keysOpt = require('./config/keys');
 const client = new google.auth.JWT(
     // https://developers.google.com/identity/protocols/googlescopes
     keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']
 );
 
 
+
+// AUTHENTICATING TO GOOGLE SPRIDE SHEET
 client.authorize((err, tokens)=>{
     if(err){
         console.log(err);    
@@ -21,14 +24,25 @@ client.authorize((err, tokens)=>{
 });
 
 
+
+
+/*
+USEING CLIENT AUTH,
+GET DATA FROM SHEET, 
+MAKE A GET REQUEST
+*/
 gsrun = async (cl)=>{
+
     const gsapi = google.sheets({version: 'v4', auth: cl});
+
+    // SETTING OPTIONS FOR GET METHOD
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get
     const opt = {
-        spreadsheetId : '1Ntr1iWR7mtHUnGSklkqobhMqLEwbuKQIfuvzce4D740',
+        spreadsheetId : keysOpt.spreadsheetId,
         range: 'A8:K16'
     };
 
     let data = await gsapi.spreadsheets.values.get(opt);
-    console.log(data);
-    
+    console.log("data properties: " + data);
+    console.log("values fo data: " + data.data.values);    
 }
